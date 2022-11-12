baaa588274994e3db6f8ed6842217d82fba68a2e
ayushh-gautam <ayushgautam896@gmail.com>
2022-11-12 23:32:51 +0545

    initial commit

---
+++ lib/Pages/mainpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class mainpage extends StatelessWidget {
+  const mainpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      // body: Container(child: BottomNavigationBar(),),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'Pages/mainpage.dart';
+
+void main(List<String> args) {
+  runApp( const MaterialApp(
+    home:  mainpage(),
+  ));
+}
